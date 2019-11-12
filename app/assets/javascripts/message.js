096c4b037dadf1e2e87dae4071280a540ea1bcd1
arikawa0831 <mates.arikawa@gmail.com>
2019-11-12 18:31:11 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,9 +5,9 @@ $(document).on('turbolinks:load', function(){
     var content = message.content ? `<p class="lower-message__content">
                                        ${message.content}
                                      </p>` : "";
-    var image = message.image ? `<img class="lower-message__image" src="${message.image}" alt="">` : "";
+    var image = message.image.url ? `<img class="lower-message__image" src="${message.image.url}" alt="">` : "";
 
-    var html = `<div class="chat__main__message__box">
+    var html = `<div class="chat__main__message__box" data-id="${message.id}">
                   <div class="chat__main__message__box__top">
                     <div class="chat__main__message__box__top__name">
                       ${message.name}
@@ -51,5 +51,33 @@ $(document).on('turbolinks:load', function(){
       $('.chat__main__footer__form__send').removeAttr("disabled")
     })
   })
-})
+
+  var reloadMessages = function() {
+    const last_message_id = $('.chat__main__message__box:last').data('id');
+    const gid = $('.chat__main__header__left__gname').data('gid');
+
+    $.ajax({
+      url: "/groups/"+gid+"/api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+
+    .done(function(messages) {
+      messages.forEach(function(message){
+        var insertHTML = []; 
+        var insertHTML = buildMessage(message); 
+        $('.chat__main__message').append(insertHTML);
+        $('.chat__main__message').animate({ scrollTop: $('.chat__main__message')[0].scrollHeight });
+      })
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
+  setInterval(reloadMessages, 7000);
+  
+});
+
 
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,7 @@
-json.content @message.content
-json.image @message.image.url
-json.name @message.user.name
+# json.content @message.content
+# json.image @message.image.url
+
+json.(@message, :content, :image)
 json.time @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.name @message.user.name
+json.id @message.id
