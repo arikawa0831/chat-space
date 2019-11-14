9c6ab974ee931707f66fb28549a9b74554584f6c
arikawa0831 <mates.arikawa@gmail.com>
2019-11-14 11:27:57 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 console.log("test");
 
 $(function(){
-  console.log("aaaaaa");
   function buildMessage(message){
     var content = message.content ? `<p class="lower-message__content">
                                        ${message.content}
@@ -30,7 +29,6 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    console.log(this);
     var url = $(this).attr('action');
     $.ajax({
       url: url,
